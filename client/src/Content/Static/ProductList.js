7391f6f0e79b207e020acb1ebea3805b49372dcf
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2022-07-24 01:06:34 +0430

    Update ProductList.js

--- client/src/Content/Static/ProductList.js
+++ client/src/Content/Static/ProductList.js
@@ -3,7 +3,7 @@ let ProductList = [
     id: 1,
     name: "Ambiguity in the financial markets",
     description:
-      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the ... ",
+      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the...",
     image:
       "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2021/09/stockmarket-1631258083.jpg",
     link: "/Page1",
@@ -13,7 +13,7 @@ let ProductList = [
     id: 2,
     name: "Ambiguity in the financial markets",
     description:
-      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the ... ",
+      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the...",
     image:
       "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2021/09/stockmarket-1631258083.jpg",
     link: "/Page1",
@@ -23,7 +23,7 @@ let ProductList = [
     id: 3,
     name: "Ambiguity in the financial markets",
     description:
-      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the ... ",
+      "Numerous machine learning techniques employ stochastic optimization to optimize the expected performance or loss, such as the mean squared error for regressions or the...",
     image:
       "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2021/09/stockmarket-1631258083.jpg",
     link: "/Page1",
